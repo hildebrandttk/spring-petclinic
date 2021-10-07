73bc8d945d59bfc43f8cb868e436e3f419f8e071
Stefan Hildebrandt <stefan.hildebrandt.ext@ewe.de>
2021-10-07 17:15:56 +0200

    Add baseUrl to config

--- src/test/groovy/GebConfig.groovy
+++ src/test/groovy/GebConfig.groovy
@@ -1,9 +1,10 @@
 import io.github.bonigarcia.wdm.WebDriverManager
 import io.github.bonigarcia.wdm.config.DriverManagerType
 import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.firefox.FirefoxDriver
 
 cacheDriver = true
+baseUrl = 'http://localhost:8080'
+
 waiting {
    timeout = 5
 }

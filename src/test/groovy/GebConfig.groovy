73c506163a6caa15d19598c87006beefb92df22a
Stefan Hildebrandt <stefan.hildebrandt.ext@ewe.de>
2021-10-07 20:07:23 +0200

    Integration test full

--- src/test/groovy/GebConfig.groovy
+++ src/test/groovy/GebConfig.groovy
@@ -2,7 +2,7 @@ import org.springframework.samples.petclinic.geb.CreateOwnerTest
 
 cacheDriver = true
 //baseUrl = 'http://localhost:8080'
-baseUrl = "http://host.testcontainers.internal:8080"
+baseUrl = "http://petclinic:8080"
 waiting {
    timeout = 5
 }

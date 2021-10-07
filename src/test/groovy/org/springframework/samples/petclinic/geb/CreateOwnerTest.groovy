543cad49e7634c65c3f21d02bed2622721fd6196
Stefan Hildebrandt <stefan.hildebrandt.ext@ewe.de>
2021-10-07 18:57:50 +0200

    Execute browser with testcontainers

--- src/test/groovy/org/springframework/samples/petclinic/geb/CreateOwnerTest.groovy
+++ src/test/groovy/org/springframework/samples/petclinic/geb/CreateOwnerTest.groovy
@@ -2,12 +2,34 @@ package org.springframework.samples.petclinic.geb
 
 import geb.junit5.GebTest
 import org.junit.jupiter.api.Test
+import org.openqa.selenium.chrome.ChromeOptions
+import org.slf4j.LoggerFactory
 import org.springframework.samples.petclinic.geb.pages.CreateOwnerPage
 import org.springframework.samples.petclinic.geb.pages.HomePage
 import org.springframework.samples.petclinic.geb.pages.OwnerDetailPage
 import org.springframework.samples.petclinic.geb.pages.OwnerSearchPage
+import org.testcontainers.containers.BrowserWebDriverContainer
+import org.testcontainers.containers.VncRecordingContainer
+import org.testcontainers.containers.output.Slf4jLogConsumer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
+@Testcontainers
 class CreateOwnerTest extends GebTest {
+
+   static {
+      //Mapping of localhost port on host computer to all containers
+      org.testcontainers.Testcontainers.exposeHostPorts(8080);
+   }
+
+   @Container
+   static BrowserWebDriverContainer webDriverContainer = (BrowserWebDriverContainer) new BrowserWebDriverContainer()
+      .withCapabilities(new ChromeOptions())
+      .withRecordingMode(
+         BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL,
+         new File("target"),
+         VncRecordingContainer.VncRecordingFormat.MP4)
+      .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("Selenium-Container")))
 
    @Test
    void testCreateOwner(){
